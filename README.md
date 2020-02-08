3af65e53c0dc959b8058f3e36ef54e170b123ab2
Hikaru Terazono (3c1u) <3c1u@vulpesgames.tokyo>
2020-02-09 00:50:47 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## modoki-ui
+
+A GUI frontend for Modoki PaaS. See [here](https://github.com/modoki-paas/modoki-k8s) for more information.
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h2>うやん！</h2>
+    </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator"
+
+@Component
+export default class App extends Vue{
+}
+</script>
